76afb0e593ec98c39c9d3ca58139ad0fbc71eadf
Anthony Thibault <ajt.hyperlogic.org>
2009-01-22 15:54:57 -0800

    Updated makefile for linux, sound doesn't work tho

--- src/system/glut/sound.cpp
+++ src/system/glut/sound.cpp
@@ -2,56 +2,16 @@
 #include "../system.h"
 #include <stdio.h>
 
-void SYS_InitSound()
-{
-	// TODO:
-}
-
-void SYS_ShutdownSound()
-{
-	// TODO:
-}
-
-SYS_SOUNDHANDLE SYS_LoadSound( char* fileName )
-{
-	// TODO:
-	return 0;
-}
-
-void SYS_ReleaseSound( SYS_SOUNDHANDLE soundHandle )
-{
-	// TODO:
-}
-
-// plays this sound once, (fire and forget)
-void SYS_TriggerSound( SYS_SOUNDHANDLE soundHandle )
-{
-	// TODO:
-}
-
-void SYS_TriggerSoundVolume( SYS_SOUNDHANDLE soundHandle, unsigned char volume, unsigned char pan )
-{
-	// TODO:
-}
-
-// play this sound but only one instance of this sound is allowed to be playing at once.
-void SYS_PlaySound( SYS_SOUNDHANDLE soundHandle, int looping )
-{
-	// TODO:
-}
-
-void SYS_PlaySoundVolume( SYS_SOUNDHANDLE soundHandle, int looping, unsigned char volume, unsigned char pan )
-{
-	// TODO:
-}
-
-void SYS_StopSound( SYS_SOUNDHANDLE soundHandle )
-{
-	// TODO:
-}
-
-int SYS_IsSoundPlaying( SYS_SOUNDHANDLE soundHandle )
-{
-	// TODO:
-	return 0;
-}
+void SYS_InitSound() {}
+void SYS_ShutdownSound() {}
+void SYS_ProcessSound() {}
+SYS_SOUNDHANDLE SYS_LoadSound( char* fileName ) {}
+void SYS_ReleaseSound( SYS_SOUNDHANDLE soundHandle ) {}
+void SYS_TriggerSound( SYS_SOUNDHANDLE soundHandle ) {}
+void SYS_TriggerSoundVolume( SYS_SOUNDHANDLE soundHandle, unsigned char volume ) {}
+void SYS_SetListenerOrientation( float position[3], float forward[3], float up[3] ) {}
+void SYS_TriggerSound3D( SYS_SOUNDHANDLE soundHandle, float position[3], unsigned char volume ) {}
+void SYS_PlaySound( SYS_SOUNDHANDLE soundHandle, int looping ) {}
+void SYS_PlaySoundVolume( SYS_SOUNDHANDLE soundHandle, int looping, unsigned char volume ) {}
+void SYS_StopSound( SYS_SOUNDHANDLE soundHandle ) {}
+int SYS_IsSoundPlaying( SYS_SOUNDHANDLE soundHandle ) {}
